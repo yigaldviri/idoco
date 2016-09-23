cdbf8bf6984ece07831ec3a5908ff99d4d56aa5e
Sharon Hophy <sharon.hophy@ca.com>
2016-09-23 14:20:39 +0300

    display profile at start

--- idoco.js
+++ idoco.js
@@ -1,6 +1,6 @@
 angular.module('idoco',[])
 	.controller("Ctrl", function($scope){
-
+		$scope.state = 'profile';
         $scope.isActive = function() {
 			console.log("state is: " + $scope.state);
 			return $scope.state === 'song';
