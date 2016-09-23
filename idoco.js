2b7e99be01eb778c6b62f75d5c144b195d77d7b7
Dviri_Yigal <yigal.dviri@ca.com>
2016-09-23 11:31:53 +0300

    Adding profile page

--- idoco.js
+++ idoco.js
@@ -1,6 +1,12 @@
 angular.module('idoco',[])
-	.controller("Ctrl", function($scope){
-		$scope.isActive = function() {
+	.controller("Ctrl", function($scope, $timeout, $location){
+
+        //initial state
+        $timeout( function() {
+            $scope.state = 'profile';
+            $location.path('/profile')
+        },1000);
+        $scope.isActive = function() {
 			console.log("state is: " + $scope.state);
 			return $scope.state === 'song';
 		};
